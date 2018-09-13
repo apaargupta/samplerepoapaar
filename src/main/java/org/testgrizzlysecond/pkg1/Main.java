b9e6747a09422486c399edfd375f5172bec032bc
Apaar Gupta <apargupta30891@yahoo.co.in>
2018-09-13 20:26:16 +0530

    init commit 2

---
+++ src/main/java/org/testgrizzlysecond/pkg1/Main.java
@@ -0,0 +1,24 @@
+package org.testgrizzlysecond.pkg1;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        org.glassfish.jersey.server.ResourceConfig rc = new ResourceConfig();
+        rc.registerClasses(org.testgrizzlysecond.pkg1.DummyController.class);
+        URI uri = URI.create("http://localhost");
+        HttpServer webServer = GrizzlyHttpServerFactory.createHttpServer(uri, rc, false);
+        System.out.println("Server Created");
+        try {
+            webServer.start();
+            System.out.println("Server Started");
+        } catch (IOException e) {
+        }
+    }
+}
